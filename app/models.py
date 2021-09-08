23b18ff6d1eaaf1f9eeaef81a01680e072ea6ccd
pwu <pwu@eecs.wsu.edu>
2021-09-08 11:46:18 -0700

    Version V.04 - Implemented user registration.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
+from enum import unique
 from app import db
+from datetime import date
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Class(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,3 +22,22 @@ class Major(db.Model):
     classes = db.relationship('Class',backref = 'coursemajor', lazy = 'dynamic')
     def __repr__(self):
         return '<Major name: {} - department: {}>'.format(self.name,self.department)
+
+class Student (db .Model):
+    id =db.Column(db.Integer, primary_key = True)
+    username =db.Column(db.String(64),unique=True, index =True)
+    password_hash =db.Column(db.String(128))
+    firstname =db.Column(db.String(100))
+    lastname = db.Column(db.String(100))
+    address=db.Column(db.String(200))
+    email=db.Column(db.String(120),unique=True,index=True)
+    last_seen = db.Column(db.DateTime,default=datetime.utcnow)
+
+def _repr_ (self):| 
+    return '<Student : {}---{}.{}.-{};>'.format (self.id, self.firstname, self.lastname, self.email)
+
+def set_ password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+def check_ password(self, password):
+    return check_password_hash(self.password_hash, password)
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,49 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app,db
 
-from app.forms import ClassForm
-from app.models import Class, Major
+from app.forms import ClassForm, RegistrationForm
+from app.models import Class, Major, Student
 
 @app.before_first_request
 def initDB(*args, **kwargs):
     db.create_all()
+    if Major.query.count() == 0:
+    majors = [{'name':'CptS','department': 'School of EECS'},
+              {'name':'SE ','department':'Schoolof EECS '},
+              {'name':'EE','department':'School of EECS'},
+              {'name':'ME ','department':' Mechanical Engineering'},'
+              {'name':'MATH',' department':'Mathematics'}£¬ ]
+    for t in majors:
+    db.session.add(Major(name=t['name'], department=t['department']))
+    db.session.commit()
+
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
     allclasses = Class.query.order_by(Class.major).all()
     return render_template('index.html', title="Course List", classes = allclasses)
+
+@app.route('/createclass/', methods=['GET' , 'POST'])
+def createclass():
+    cform = ClassForm()
+    if cform.validate_on_submit():
+          newClass = Class(coursenum = cform.coursenum.data, title= cform.title.data, major= cform.major.data.name )
+          db.session.add(newClass)
+          db.session.commit()
+          flash('Class"' + newClass.major + '-' + newClass.coursenum + '"is created')
+          return redirect(url_for('index'))
+
+return render_template('create_class.html', form = cform)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    rform = RegistrationForm()
+    if rform.validate_on_submit():
+        student = Student(username= rform.username.data, email=rform.email.data, firstname = rform.firstname.data, lastname = rform.lastname.data, address = rform.address.data)
+        student.set_ password(rform.password.data)
+        db.session.add(student)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('index'))
+    return render_template('register.html', form =rform)
