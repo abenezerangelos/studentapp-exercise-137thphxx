74b55e91d56ce007e02f77c68bad8578302b5bbb
pwu <pwu@eecs.wsu.edu>
2021-09-08 22:10:30 -0700

    Version 0.13. Add Bootstrap styling.

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_moment import Moment
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
